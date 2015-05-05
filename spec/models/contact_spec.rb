3a9542ea846459560afbb707c65076263311a597
Tshinori Sugita <sugita@c-fo.com>
2015-05-05 23:38:36 +0900

    add test case

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -7,14 +7,25 @@ describe Contact do
       email: 'tester@exemple.com')
     expect(contact).to be_valid
   end
+
   it "is invalid without a firstname" do
     expect(Contact.new(firstname: nil)).to have(1).errors_on(:firstname)
   end
+
   it "is invalid without a lastname" do
     expect(Contact.new(lastname: nil)).to have(1).errors_on(:lastname)
   end
 
   it "is invalid without an email address"
-  it "is invalid with a duplicate email address"
+
+  it "is invalid with a duplicate email address" do
+    Contact.create(
+        firstname: 'Joe', lastname: 'Tester',
+        email: 'tester@example.com')
+    contact = Contact.create(
+      firstname: 'Jane', lastname: 'Tester',
+        email: 'tester@example.com')
+    expect(contact).to have(1).errors_on(:email)
+  end
   it "returns a contact's full name as a string"
 end
