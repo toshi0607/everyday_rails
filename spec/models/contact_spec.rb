2b20e77ba3c26fab04d097233a433990f544927f
Tshinori Sugita <sugita@c-fo.com>
2015-05-06 00:50:05 +0900

    use before :each

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -36,25 +36,25 @@ describe Contact do
     expect(contact.name).to eq 'John Doe'
   end
 
-  it "returns a sorted array of results that match" do
-    smith = Contact.create(firstname: 'John', lastname: 'Smith',
-      email: 'jsmith@example.com')
-    jones = Contact.create(firstname: 'Tim', lastname: 'Jones',
-      email: 'tjones@example.com')
-    johnson = Contact.create(firstname: 'John', lastname: 'Johnson',
-      email: 'jjohnson@example.com')
+  describe "filter last name by letter" do
+    before :each do
+      @smith = Contact.create(firstname: 'John', lastname: 'Smith',
+        email: 'jsmith@example.com')
+      @jones = Contact.create(firstname: 'Tim', lastname: 'Jones',
+        email: 'tjones@example.com')
+      @johnson = Contact.create(firstname: 'John', lastname: 'Johnson',
+        email: 'jjohnson@example.com')
+    end
+    context "matching letters" do
+      it "returns a sorted array of results that match" do
+        expect(Contact.by_letter('J')).to eq [@johnson, @jones]
+      end
+    end
 
-    expect(Contact.by_letter("J")).to eq [johnson, jones]
-  end
-
-  it "returns a sorted array of results that match" do
-    smith = Contact.create(firstname: 'John', lastname: 'Smith',
-                           email: 'jsmith@example.com')
-    jones = Contact.create(firstname: 'Tim', lastname: 'Jones',
-                           email: 'tjones@example.com')
-    johnson = Contact.create(firstname: 'John', lastname: 'Johnson',
-                             email: 'jjohnson@example.com')
-
-    expect(Contact.by_letter("J")).to_not include smith
+    context "non-matching letters" do
+      it "returns a sorted array of results that match" do
+        expect(Contact.by_letter("J")).to_not include @smith
+      end
+    end
   end
 end
