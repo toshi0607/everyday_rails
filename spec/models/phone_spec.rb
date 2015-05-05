8442f3a383a0f446d87f429cbeb86923c250f7e9
Tshinori Sugita <sugita@c-fo.com>
2015-05-05 23:44:51 +0900

    add test case

---
+++ spec/models/phone_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Phone do
+  it "does not allow duplicate phone number per contact" do
+    contact = Contact.create(firstname: 'Joe', lastname: 'Tester',
+      email: 'joetester@example.com')
+    home_phone = contact.phones.create(phone_type: 'home',
+      phone: '785-555-1234')
+    mobile_phone = contact.phones.build(phone_type: 'mobile',
+      phone: '785-555-1234')
+
+    expect(mobile_phone).to have(1).errors_on(:phone)
+  end
+end
