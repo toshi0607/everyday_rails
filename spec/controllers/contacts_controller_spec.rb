746d106326f80c0cbe8133df8b49581dbc2b7495
Tshinori Sugita <sugita@c-fo.com>
2015-05-06 12:18:54 +0900

    add test case

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -55,7 +55,11 @@ describe ContactsController do
   end
 
   describe 'GET #edit' do
-    it "assigns the requested contact to @contact"
+    it "assigns the requested contact to @contact"do
+      contact = create(:contact)
+      get :edit, id: contact
+      expect(assigns(:contact)).to eq contact
+    end
     it "renders the :edit template"
   end
 
