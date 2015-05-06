2065a05ec466cb29002d4301035bcd97fb4d232c
Tshinori Sugita <sugita@c-fo.com>
2015-05-06 10:08:11 +0900

    refactor spec

--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -13,14 +13,11 @@ describe Phone do
   end
 
   it "allows two contacts to share a phone number" do
-    contact = Contact.create(firstname: 'Joe', lastname: 'Tester',
-      email: 'joetester@example.com')
-    contact.phones.create(phone_type: 'home',
-      phone: '785-555-1234')
-    other_contact = Contact.new
-    other_phone = other_contact.phones.build(phone_type:
-      'home', phone: '785-555-1234')
-
-    expect(other_phone).to be_valid
+    create(:phone,
+      phone_type: 'home',
+      phone: "785-555-1234")
+    expect(build(:phone,
+      phone_type: 'home',
+      phone: "785-555-1234")).to be_valid
   end
 end
